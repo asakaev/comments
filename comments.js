3b4879fb0d8e08bf57029d270e86f9cb9dad8271
tema <tema@vf8.ru>
2014-05-11 03:56:32 +0400

    button handler

--- comments.js
+++ comments.js
@@ -18,7 +18,9 @@ function showRecursive(data, place, num, reclvl) {
         str = '<div class="media">';
         str += '<a class="pull-left" href="#"><img class="media-object" src="/comments/post.png" alt="Media Object"></a>';
         str += '<div class="post media-body" id="' + num + '">';
-        str += '<h4 class="media-heading">' + data.self.title + '</h4>' + data.self.post + '</div></div>';
+        str += '<h4 class="media-heading">' + data.self.title + '</h4>' + data.self.post;
+        str += ' <button type="button" class="btn btn-info btn-xs" id="btn' + data.self.id + '"' + '>Reply</button>';
+        str += '</div></div>';
         el = $(str);
         place.append(el);
         place = el;
@@ -34,11 +36,18 @@ function showRecursive(data, place, num, reclvl) {
         str += '<a class="pull-left" href="#"><img class="media-object" src="/comments/comment.png" alt="Media Object"></a>';
         str += '<div' + recStr + idStr + '><h4 class="media-heading">';
         str += data.self.name + ' (' + data.self.email + ')</h4>';
-        str += data.self.message + '</div>';
+        str += data.self.message;
+        str += ' <button type="button" class="btn btn-info btn-xs" id="btn' + data.self.id + '"' + '>Reply</button>';
+        str += '</div>';
         el = $(str);
         place.append(el);
         place = el;
     }
+
+    // button handle
+    $('#btn' + data.self.id).click(function(){
+        console.log('checkut: ' + data.self.id);
+    });
 
     // if no childs
     if (data.childs.length == 0) {
