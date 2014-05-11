43f8e055b0d8207b3e21705df2485a941ab83d32
tema <tema@vf8.ru>
2014-05-11 11:27:38 +0400

    URLs as hyperlinks.

--- process.php
+++ process.php
@@ -1,5 +1,12 @@
 <?php
 
+function MakeUrls($str)
+{
+	$find=array('`((?:https?|ftp)://\S+[[:alnum:]]/?)`si','`((?<!//)(www\.\S+[[:alnum:]]/?))`si');
+	$replace=array('<a href="$1" target="_blank">$1</a>','<a href="http://$1"    target="_blank">$1</a>');
+	return preg_replace($find,$replace,$str);
+}
+
 header('Content-Type: application/json');
 
 if (isset($_POST['name'])) {
@@ -7,6 +14,8 @@ if (isset($_POST['name'])) {
 	$email = strip_tags($_POST['email']);
 	$message = strip_tags($_POST['message']);
 	$parent = strip_tags($_POST['parentId']);
+
+	$message = MakeUrls($message);
 
 	$con=mysqli_connect("localhost","root","1234","test");
 	// Check connection
