d654aa86c1ddc927637abb509f037694f6560f71
tema <tema@vf8.ru>
2014-05-11 06:41:17 +0400

    Before modal window.

--- comments.js
+++ comments.js
@@ -1,7 +1,7 @@
 var str, el;
 
 function loadJSON() {
-    $.getJSON('/comments/getcomments', function (data) {
+    $.getJSON('getcomments', function (data) {
         console.log(data);
         var postCount = 1;
         $.each(data, function (key, val) {
@@ -16,14 +16,13 @@ function showRecursive(data, place, num, reclvl) {
     // if post
     if (data.self.parent == 0) {
         str = '<div class="media">';
-        str += '<a class="pull-left" href="#"><img class="media-object" src="/comments/post.png" alt="Media Object"></a>';
+        str += '<a class="pull-left" href="#"><img class="media-object" src="post.png" alt="Media Object"></a>';
         str += '<div class="post media-body" id="' + num + '">';
         str += '<h4 class="media-heading">' + data.self.title + '</h4>' + data.self.post;
         str += ' <button type="button" class="btn btn-info btn-xs" id="btn' + data.self.id + '"' + '>Reply</button>';
         str += '</div></div>';
         el = $(str);
         place.append(el);
-        place = el;
 
         // to append comments under post with left padding
         var numStr = '"#' + num + '"';
@@ -33,7 +32,7 @@ function showRecursive(data, place, num, reclvl) {
         var recStr = ' class="comment media-body lvl' + reclvl + '"';
         var idStr = ' id="' + num + '"';
         str = '<div class="media">';
-        str += '<a class="pull-left" href="#"><img class="media-object" src="/comments/comment.png" alt="Media Object"></a>';
+        str += '<a class="pull-left" href="#"><img class="media-object" src="comment.png" alt="Media Object"></a>';
         str += '<div' + recStr + idStr + '><h4 class="media-heading">';
         str += data.self.name + ' (' + data.self.email + ')</h4>';
         str += data.self.message;
@@ -45,9 +44,10 @@ function showRecursive(data, place, num, reclvl) {
     }
 
     // button handle
-    $('#btn' + data.self.id).click(function(){
-        console.log('checkut: ' + data.self.id);
+    $('#btn' + data.self.id).click(function () {
+        $('#btn' + data.self.id).parent().parent().html('test');
     });
+
 
     // if no childs
     if (data.childs.length == 0) {
